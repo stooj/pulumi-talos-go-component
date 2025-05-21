a9f2c923515c07b42081b196d1c6ec94cab65f3a
stoo johnston <stoo@pulumi.com>
2025-05-21 12:58:12 +0100

    switch main to infer component

--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
 import (
-	"talos-go-component/pkg/talos"
+	"github.com/dirien/pulumi-talos-go-component/pkg/talos"
 
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi-go-provider/infer"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		component, err := talos.NewTalosCluster(ctx, "talos-cluster", talos.TalosClusterArgs{
-			ClusterName:       pulumi.String("talos-cluster"),
-			Version:           pulumi.String("v1.9.5"),
-			Region:            pulumi.String("lon1"),
-			CountControlPlane: 3,
-			CountWorker:       1,
-			Size:              pulumi.String("s-2vcpu-4gb"),
-		})
-		if err != nil {
-			return err
-		}
-		ctx.Export("kubeconfig", component.Kubeconfig)
-		return nil
-	})
+	err := infer.NewProviderBuilder().
+		WithName("go-components").
+		WithNameSpace("stooj").
+		WithComponents(
+			infer.Component(talos.NewTalosCluster),
+		).
+		BuildAndRun()
+	if err != nil {
+		panic(err)
+	}
 }
